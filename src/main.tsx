40bc1e744394b4b104149c753ce46b29c1fd23a6
Jihoon Lee <anteater1056@gmail.com>
2025-05-27 07:17:55 +0900

    init: 천리길도 한 걸음 부터

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+/**
+ * 엔트리 포인트
+ */
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
