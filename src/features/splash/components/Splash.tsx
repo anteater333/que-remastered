540b12d62c231b9303822db30624f65bacc71eaf
Jihoon Lee <anteater1056@gmail.com>
2025-06-02 07:07:33 +0900

    init: 스플래시 이미지 표시

---
+++ src/features/splash/components/Splash.tsx
@@ -0,0 +1,7 @@
+import splash from "../assets/splash.png";
+
+const Splash = () => {
+  return <img src={splash} alt="splash"></img>;
+};
+
+export default Splash;
--- src/features/splash/index.tsx
+++ src/features/splash/index.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Splash from "./components/Splash";
 
 type QueSplashProps = {
   children: ReactNode;
@@ -7,10 +8,15 @@ type QueSplashProps = {
 /**
  * Que 어플리케이션의 시작을 알리는 스플래시 화면을 담당.
  * 스플래시 화면을 표출한 다음 어플리케이션 시작을 위한 준비를 합니다.
- * 최소 표시 시간 경과 & 어플리케이션 준비가 완료되면 스플리시 화면을 종료합니다.
+ * 최소 표시 시간 경과 & 어플리케이션 준비가 완료되면 스플래시 화면을 종료합니다.
  */
 const QueSplashProvider = ({ children }: QueSplashProps) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Splash />
+      {children}
+    </>
+  );
 };
 
 export default QueSplashProvider;
