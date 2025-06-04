a340d2dd740acae56d69b11a0070313545fb7f52
Jihoon Lee <anteater1056@gmail.com>
2025-06-05 08:21:14 +0900

    init: 스플래시는 장기적으로 이미지가 아닌 CSS로 바꿔볼 예정

--- src/features/splash/components/Splash.tsx
+++ src/features/splash/components/Splash.tsx
@@ -4,7 +4,11 @@ import splash from "../assets/splash.png";
  * 스플래시 화면에 대한 시각적 컴포넌트
  */
 const Splash = () => {
-  return <img src={splash} alt="splash"></img>;
+  return (
+    <div className="splash-container">
+      <img src={splash} alt="splash"></img>
+    </div>
+  );
 };
 
 export default Splash;
