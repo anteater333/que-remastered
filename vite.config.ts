e9d2bab149c0708c4133bcba9135e2d371871521
Jihoon Lee <anteater1056@gmail.com>
2025-06-08 18:43:15 +0900

    init: WSL에서 Mobile Web First 개발을 한다면

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,45 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: "print-wsl-host-ip",
+        configureServer() {
+          if (env.VITE_WSL_HOST_IP) {
+            console.log("🥕=============================================🥕");
+            console.log("  HAPPY😭 HACKING W/ WSL !!");
+            console.log(`  WSL Host: http://${env.VITE_WSL_HOST_IP}:5173`);
+            console.log("🥕=============================================🥕");
+          }
+        },
+      },
+    ],
+    css: {
+      modules: {
+        localsConvention: "camelCase",
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
         @use '@/styles/common' as *;
         `,
+        },
       },
     },
-  },
-  server: {
-    host: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    server: {
+      host: true,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  };
 });
