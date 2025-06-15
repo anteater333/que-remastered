713cd959a6c97246f40943c3c2cd984498c43eb6
Jihoon Lee <anteater1056@gmail.com>
2025-06-16 06:55:02 +0900

    init: QR 코드를 통한 좀 더 편한 개발 환경 구축하기 (WSL 한정)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import QRCode from "qrcode";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,12 +12,17 @@ export default defineConfig(({ mode }) => {
       react(),
       {
         name: "print-wsl-host-ip",
-        configureServer() {
+        async configureServer() {
           if (env.VITE_WSL_HOST_IP) {
-            console.log("🥕=============================================🥕");
-            console.log("  HAPPY😭 HACKING W/ WSL !!");
-            console.log(`  WSL Host: http://${env.VITE_WSL_HOST_IP}:5173`);
-            console.log("🥕=============================================🥕");
+            const URL = `http://${env.VITE_WSL_HOST_IP}:5173`;
+            const QR = await QRCode.toString(URL);
+
+            setTimeout(() => {
+              console.log("🥕=============================================🥕");
+              console.log("  HAPPY😭 HACKING W/ WSL !!");
+              console.log(`  WSL Host: ${URL}`);
+              console.log(`    ${QR.trim()}`);
+            }, 500);
           }
         },
       },
