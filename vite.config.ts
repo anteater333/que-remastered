34cf7428fceceb913cda2e222c7353849ea18f2b
Jihoon Lee <anteater1056@gmail.com>
2025-06-17 06:49:54 +0900

    feat: 탠스택 라우터 재설정 시도 중
    실시간 이슈를 만나버려서 일단 여기까지만 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import QRCode from "qrcode";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
+      tanstackRouter(),
       react(),
       {
         name: "print-wsl-host-ip",
