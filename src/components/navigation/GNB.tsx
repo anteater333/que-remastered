c94f64e026fa99c9b2d17543f44d313c6e60dc86
Jihoon Lee <anteater1056@gmail.com>
2025-09-24 09:29:18 +0900

    feat: GNB 레이아웃 잡는 중

--- src/components/navigation/GNB.tsx
+++ src/components/navigation/GNB.tsx
@@ -2,17 +2,19 @@ import styles from "./navigation.module.scss";
 import { IcoSearch } from "../common/icon/IcoSearch";
 import { IcoNoti } from "../common/icon/IcoNoti";
 import logo from "@/assets/custom/haeder-logo.png";
+import { Profile } from "../common/profile/Profile";
 
 export const GNB = () => {
   return (
     <div id="header" className={styles.gnb}>
-      <div>
+      <div className={styles.gnbLeft}>
         <img src={logo} />
       </div>
 
-      <div>
+      <div className={styles.gnbRight}>
         <IcoSearch className={""} />
         <IcoNoti className={""} />
+        <Profile />
       </div>
     </div>
   );
