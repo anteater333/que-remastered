87c198b7321973296dd8c20e7cc7517fc1bfed85
Jihoon Lee <anteater1056@gmail.com>
2025-09-28 16:36:08 +0900

    feat: GNB 네비게이션 추가

--- src/components/navigation/GNB.tsx
+++ src/components/navigation/GNB.tsx
@@ -3,18 +3,27 @@ import { IcoSearch } from "../common/icon/IcoSearch";
 import { IcoNoti } from "../common/icon/IcoNoti";
 import logo from "@/assets/custom/haeder-logo.png";
 import { Profile } from "../common/profile/Profile";
+import { Link } from "@tanstack/react-router";
 
 export const GNB = () => {
   return (
     <div id="header" className={styles.gnb}>
       <div className={styles.gnbLeft}>
-        <img src={logo} />
+        <Link className={styles.logo} to="/">
+          <img src={logo} />
+        </Link>
       </div>
 
       <div className={styles.gnbRight}>
-        <IcoSearch className={""} />
-        <IcoNoti className={""} />
-        <Profile />
+        <Link to="/search">
+          <IcoSearch className={""} />
+        </Link>
+        <Link to="/alert">
+          <IcoNoti className={""} />
+        </Link>
+        <Link to="/studio">
+          <Profile />
+        </Link>
       </div>
     </div>
   );
