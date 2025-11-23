3db90f0d983a95d4a9aba2de84ce60e81926f420
Jihoon Lee <anteater1056@gmail.com>
2025-11-24 08:51:17 +0900

    feat: 캐치프레이즈

--- src/features/intro/index.tsx
+++ src/features/intro/index.tsx
@@ -3,6 +3,9 @@ import styles from "./intro.module.scss";
 import { SignupButton } from "./components/SignupButton";
 import { LogoText } from "../../components/common/logo/LogoText";
 
+/** TODO: 어드민 설정 가능하도록 만들기 */
+const CATCHPHRASE = `내 방에서 열리는 <span style="color: #07b2f8">콘서트</span>`;
+
 const Intro = () => {
   return (
     <div className={styles.introRoot}>
@@ -12,15 +15,15 @@ const Intro = () => {
         <div className={styles.logo}>
           <LogoText isColored={true} />
         </div>
-        <div className={styles.catchPhrase}>
-          {/* todo: 캐치 프레이즈 변경 */}
-          당신의 콘서트를 시작하세요
-        </div>
+        <div
+          className={styles.catchPhrase}
+          dangerouslySetInnerHTML={{ __html: CATCHPHRASE }}
+        ></div>
         <div className={styles.buttonGroup}>
           <SignupButton />
         </div>
         <div className={styles.login}>
-          <p>이미 게정이 있다면</p>
+          <p>이미 계정이 있다면</p>
           <Link to="/login">로그인</Link>
         </div>
       </div>
