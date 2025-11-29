844a04db96f48f1e945750957b0bc2545cc48a51
anteater@mac <anteater1056@gmail.com>
2025-11-29 15:41:19 +0900

    feat: 인트로 페이지 레이아웃 완성

--- src/features/intro/components/SignupButton.tsx
+++ src/features/intro/components/SignupButton.tsx
@@ -1,12 +1,13 @@
 import { Link } from "@tanstack/react-router";
+import styles from "./SignupButton.module.scss";
 
 export interface SignupButton {}
 
 export const SignupButton = ({}: SignupButton) => {
   return (
-    <Link to="/signup">
-      <p>Q</p>
-      <p>QUE 계정 만들기</p>
+    <Link className={styles.signupButton} to="/signup">
+      <span className={styles.logo}>Q</span>
+      <span className={styles.phrase}>QUE 계정 만들기</span>
     </Link>
   );
 };
--- src/features/intro/index.tsx
+++ src/features/intro/index.tsx
@@ -4,7 +4,7 @@ import { SignupButton } from "./components/SignupButton";
 import { LogoText } from "../../components/common/logo/LogoText";
 
 /** TODO: 어드민 설정 가능하도록 만들기 */
-const CATCHPHRASE = `내 방에서 열리는 <span style="color: #07b2f8">콘서트</span>`;
+const CATCHPHRASE = `내 방에서 열리는 콘서트`;
 const BG_IMG = `https://i.postimg.cc/mgdFtJd5/welcome.gif`;
 
 const Intro = () => {
@@ -16,23 +16,21 @@ const Intro = () => {
       <div className={styles.introOverlay} />
 
       <div className={styles.introContainer}>
-        <div>
-          <div className={styles.logo}>
-            <Link to="/">
-              <LogoText isColored={true} />
-            </Link>
-          </div>
-          <div
-            className={styles.catchPhrase}
-            dangerouslySetInnerHTML={{ __html: CATCHPHRASE }}
-          ></div>
-          <div className={styles.buttonGroup}>
-            <SignupButton />
-          </div>
-          <div className={styles.login}>
-            <p>이미 계정이 있다면</p>
-            <Link to="/login">로그인</Link>
-          </div>
+        <div className={styles.logo}>
+          <Link to="/">
+            <LogoText isColored={true} />
+          </Link>
+        </div>
+        <div
+          className={styles.catchPhrase}
+          dangerouslySetInnerHTML={{ __html: CATCHPHRASE }}
+        ></div>
+        <div className={styles.buttonGroup}>
+          <SignupButton />
+        </div>
+        <div className={styles.loginPhrase}>
+          <p>이미 계정이 있다면</p>
+          <Link to="/login">로그인</Link>
         </div>
       </div>
     </div>
