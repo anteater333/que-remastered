f98f4346b2cdaec762d8d4ee4c56d030107225df
Jihoon Lee <anteater1056@gmail.com>
2025-12-03 07:31:54 +0900

    feat: 공용 input 컴포넌트 구현

--- src/components/Inputs/TextInput.tsx
+++ src/components/Inputs/TextInput.tsx
@@ -1,5 +1,9 @@
-interface TextInputProps {}
+import clsx from "clsx";
+import styles from "./TextInput.module.scss";
+import type { ComponentProps } from "react";
 
-export const TextInput = ({}: TextInputProps) => {
-  return <input />;
+interface TextInputProps extends ComponentProps<"input"> {}
+
+export const TextInput = ({ className, ...props }: TextInputProps) => {
+  return <input className={clsx(styles.textInput, className)} {...props} />;
 };
--- src/features/signup/index.tsx
+++ src/features/signup/index.tsx
@@ -1,10 +1,13 @@
+import styles from "./signup.module.scss";
 import { LogoText } from "../../components/common/logo/LogoText";
+import { TextInput } from "../../components/Inputs/TextInput";
 
 const SignupPage = () => {
   return (
     <div>
       <LogoText isColored />
       <div>회원가입</div>
+      <TextInput className={styles.input} placeholder="이메일" />
     </div>
   );
 };
