f283796de8fb79a2a3846ab2ed5d87e842f1efb6
Jihoon Lee <anteater1056@gmail.com>
2026-01-07 08:33:29 +0900

    fix: Prisma 설정 수정

--- server/src/controllers/signup.controller.ts
+++ server/src/controllers/signup.controller.ts
@@ -1,4 +1,5 @@
 import { RouteHandler } from "fastify";
+import prisma from "../services/connectors/prisma.service";
 
 interface PostSignUpVerificationMailBody {
   email: string;
@@ -8,6 +9,18 @@ export const postSignUpVerificationMail: RouteHandler<{
   Body: PostSignUpVerificationMailBody;
 }> = async (request, reply) => {
   try {
+    const email = request.body.email;
+
+    if (!email) {
+      return reply.status(400).send({ message: "이메일을 입력해주세요." });
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    // 이미 사용 중인 이메일 있음
+    if (existingUser) {
+      return reply.status(409).send({ message: "이미 가입된 이메일입니다." });
+    }
+
     return reply.status(200).send({
       success: true,
       message: "인증 메일이 발송되었습니다.",
