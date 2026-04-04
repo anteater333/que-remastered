e3008ad74887ba0b32ce264340016ee9b5d92dac
anteater@mac <anteater1056@gmail.com>
2026-04-04 13:22:16 +0900

    feat: 로그 모듈 보충

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,9 +6,60 @@ import {
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import path from "path";
+
+const isProduction = process.env.NODE_ENV === "production";
+const logPath = process.env.LOG_PATH ?? __dirname;
+
+console.log("🎙️ Setting QUE API server instance...");
+console.log("🎙️ isProduction:", isProduction, process.env.NODE_ENV);
+console.log("🎙️ logPath", logPath);
 
 const server: FastifyInstance = Fastify({
-  logger: true,
+  logger: {
+    transport: {
+      targets: isProduction
+        ? [
+            {
+              target: "pino-roll",
+              level: "info",
+              options: {
+                file: path.join(logPath, "app"),
+                frequency: "daily",
+                dateFormat: "yyyyMMdd",
+                extension: ".log",
+                mkdir: true,
+                limit: {
+                  count: 30,
+                },
+              },
+            },
+            {
+              // 에러 로그 별도 저장
+              target: "pino-roll",
+              level: "error",
+              options: {
+                file: path.join(logPath, "error"),
+                frequency: "daily",
+                dateFormat: "yyyyMMdd",
+                extension: ".log",
+                mkdir: true,
+              },
+            },
+          ]
+        : [
+            {
+              target: "pino-pretty",
+              level: "info",
+              options: {
+                colorize: true,
+                translateTime: "HH:MM:ss Z",
+                ignore: "pid,hostname",
+              },
+            },
+          ],
+    },
+  },
 }).withTypeProvider<ZodTypeProvider>();
 
 // Zod 검증기 등록
