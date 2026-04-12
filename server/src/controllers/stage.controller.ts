62bed40177a55236690bf14a567e0bde8f0349a6
Jihoon Lee <anteater1056@gmail.com>
2026-04-12 16:45:51 +0900

    feat: Stage 생성 API 구현

--- server/src/controllers/stage.controller.ts
+++ server/src/controllers/stage.controller.ts
@@ -1,8 +1,22 @@
 import { RouteHandler } from "fastify";
 import { PostStageBody } from "../schemes/stage.schema";
+import prismaService from "../services/connectors/prisma.service";
 
 export const postStage: RouteHandler<{
   Body: PostStageBody;
 }> = async (request, reply) => {
-  return reply.status(501).send();
+  const { description, title } = request.body; // Note. zod 통해 검증 완료
+
+  try {
+    const newStage = await prismaService.stage.create({
+      data: { title, description, uploaderId: request.user.id },
+    });
+
+    return reply
+      .status(201)
+      .send({ message: "스테이지가 생성되었습니다.", stageId: newStage.id });
+  } catch (error) {
+    request.log.error({ error });
+    return reply.status(500).send({ message: "서버 오류" });
+  }
 };
