b62e40f70b463bc1a042b8acb871df1a41fbd371
Jihoon Lee <anteater1056@gmail.com>
2026-04-27 21:38:45 +0900

    feat: zustand를 사용한 레이아웃 내 상태 관리법

--- src/features/navigation/components/LandingGNB.tsx
+++ src/features/navigation/components/LandingGNB.tsx
@@ -1,12 +1,20 @@
 import clsx from "clsx";
 import { BackButton } from "../../../components/Buttons/BackButton";
 import styles from "./navigation.module.scss";
+import { LogoText } from "../../../components/common/logo/LogoText";
 
-export const LandingGNB = () => {
+export const LandingGNB = ({ showLogo }: { showLogo?: boolean }) => {
   return (
     <div id="header" className={clsx(styles.gnb, styles.landingGnb)}>
       <div className={styles.gnbLeft}>
         <BackButton />
+      </div>
+      <div className={styles.gnbCenter}>
+        {showLogo && (
+          <div className={styles.logo}>
+            <LogoText isColored />
+          </div>
+        )}
       </div>
       <div className={styles.gnbRight}></div>
     </div>
--- src/routes/_landingLayout.tsx
+++ src/routes/_landingLayout.tsx
@@ -1,14 +1,16 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { LandingGNB } from "../features/navigation";
+import { useLandingLayoutStore } from "../features/navigation/stores/landingLayoutStore";
 
 export const Route = createFileRoute("/_landingLayout")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const showLogo = useLandingLayoutStore((s) => s.showLogo);
   return (
     <>
-      <LandingGNB />
+      <LandingGNB showLogo={showLogo} />
       <Outlet />
     </>
   );
