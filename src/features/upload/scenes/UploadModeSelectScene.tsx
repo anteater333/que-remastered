d5586d16c506314f7f12915f64da61ba977f310e
Jihoon Lee <anteater1056@gmail.com>
2026-04-30 22:25:00 +0900

    feat: File 업로드 UI 구현

--- src/features/upload/scenes/UploadModeSelectScene.tsx
+++ src/features/upload/scenes/UploadModeSelectScene.tsx
@@ -1,15 +1,39 @@
 import styles from "./UploadModeSelectScene.module.scss";
 import { IcoUpload } from "../../../components/common/icon/IcoUpload";
+import React, { useRef, useState } from "react";
 
 const UploadModeSelectScene = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    setSelectedFile(file);
+  };
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <div className={styles.uploadModeSelectScene}>
-      <button className={styles.uploadButton}>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="video/*"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+      <button className={styles.uploadButton} onClick={handleButtonClick}>
         <div className={styles.uploadIcon}>
           <IcoUpload />
         </div>
         <div className={styles.uploadText}>파일 업로드</div>
       </button>
+      <div>Tmp: {selectedFile?.name}</div>
     </div>
   );
 };
