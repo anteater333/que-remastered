41a6beb02674c16854e924d4d6c419a88691bc7d
Jihoon Lee <anteater1056@gmail.com>
2026-06-11 21:40:23 +0900

    feat: 기본 프로필 라이브러리 추가 및 적용

--- src/features/onboarding/index.tsx
+++ src/features/onboarding/index.tsx
@@ -1,15 +1,40 @@
+import clsx from "clsx";
 import { TextArea } from "../../components/Inputs/TextArea";
 import { TextInput } from "../../components/Inputs/TextInput";
 import { SignUpFNB } from "../navigation/components/SignUpFNB";
 import styles from "./onboarding.module.scss";
+import Avatar from "boring-avatars";
+import { useForm } from "@tanstack/react-form";
 
 const OnBoardingPage = () => {
+  const form = useForm({});
+
   return (
     <>
-      <form className={styles.onBoardingContainer}>
-        <div>profile image placeholder</div>
-        <TextInput placeholder="당신의 이름은?" />
-        <TextArea placeholder="자기소개를 작성해주세요." />
+      <form id="que-onboarding" className={styles.onBoardingContainer}>
+        <div>
+          <button onClick={() => {}}>
+            <div className={styles.profileContainer}>
+              <Avatar name={"anteater333"} size={"100%"} variant="beam" />
+            </div>
+          </button>
+        </div>
+        <form.Field name="nickname">
+          {(field) => (
+            <TextInput
+              className={clsx(styles.input, styles.name)}
+              placeholder="당신의 이름은?"
+            />
+          )}
+        </form.Field>
+        <form.Field name="nickname">
+          {(field) => (
+            <TextArea
+              className={clsx(styles.input, styles.description)}
+              placeholder="자기소개를 작성해주세요."
+            />
+          )}
+        </form.Field>
       </form>
       <SignUpFNB
         isNextEnabled={true}
